14e4f9209b3fda228e3437b5555128f555a384c3
SaranNagaSai <sarannagasait@gmail.com>
2026-01-11 10:54:23 +0530

    Add docs folder for GitHub Pages hosting

---
+++ docs/js/test.js
@@ -0,0 +1,21 @@
+function finishTest() {
+  let scores = {
+    social: Number(q1.value) + Number(q2.value),
+    emotional: Number(q3.value) + Number(q4.value),
+    planning: Number(q5.value) + Number(q6.value),
+    confidence: Number(q7.value) + Number(q8.value),
+    focus: Number(q9.value) + Number(q10.value)
+  };
+
+  fetch("http://localhost:5000/api/test/submit", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ scores })
+  })
+  .then(res => res.json())
+  .then(data => {
+    localStorage.setItem("scores", JSON.stringify(scores));
+    localStorage.setItem("personality", data.personality);
+    window.location.href = "dashboard.html";
+  });
+}
